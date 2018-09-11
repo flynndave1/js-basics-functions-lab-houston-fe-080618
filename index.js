d9b75933aa43476e1144320329aa45d31f1b2d1b
david m flynn <flynndave1@msn.com>
2018-09-11 16:16:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+
+
+
+function  distanceFromHqInBlocks ( endingBlock) {
+  var startingBlock  = 42
+ var  d   =   Math.abs (  startingBlock - endingBlock ) ;
+ 
+ return d;
+}
+function  distanceFromHqInFeet (endingBlock)  {
+  var d2 = distanceFromHqInBlocks(endingBlock) ;
+  d2 *= 264 ;
+  return  d2  ;
+  
+}
+function  distanceTravelledInFeet() {
+  
+  
+}
